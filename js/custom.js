2cb9aee1fca352444f7a4b3ab901fb91b7d5571e
daniel.urbanodelarua <daniel.urbanodelarua@ext.ema.europa.eu>
2025-01-20 00:23:55 +0100

    refactor: simplify custom JavaScript initialization code

--- js/custom.js
+++ js/custom.js
@@ -6,15 +6,4 @@
  * Version: v2.0.1
  * License: Themeforest Standard Licenses: https://themeforest.net/licenses
  */
-
-/*----------- CUSTOM JS SCRIPTS -----------*/
-
-(function($) {
-  'use strict';
-  $(function() {
-    // Code here executes When the DOM is loaded...
-  });
-  $(window).on('load', function() {
-    // Code here executes When the page is loaded
-  });
-}(jQuery));
+!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
